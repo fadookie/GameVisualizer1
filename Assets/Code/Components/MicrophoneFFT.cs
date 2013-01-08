48ee33fae80e581523d97edfdfcfa74273a19270
Eliot <fadookie@gmail.com>
2013-01-08 13:58:08 -0800

    Only sample audio every 5 seconds

--- Assets/Code/Components/MicrophoneFFT.cs
+++ Assets/Code/Components/MicrophoneFFT.cs
@@ -13,36 +13,58 @@ public class MicrophoneFFT : MonoBehaviour {
     private int maxFreq;  
   
     //A handle to the attached AudioSource  
-    private AudioSource goAudioSource;  
+    private AudioSource goAudioSource = null;
+    
+	bool waitingForNextSample = true;
   
     //Use this for initialization  
-    void Start()   
-    {  
-        //Check if there is at least one microphone connected  
-        if(Microphone.devices.Length <= 0)  
-        {  
-            //Throw a warning message at the console if there isn't  
-            Debug.LogWarning("Microphone not connected!");  
-        }  
-        else //At least one microphone is present  
-        {  
-            //Set 'micConnected' to true  
-            micConnected = true;  
+    void Start ()
+	{  
+		//Check if there is at least one microphone connected  
+		if (Microphone.devices.Length <= 0) {  
+			//Throw a warning message at the console if there isn't  
+			Debug.LogWarning ("Microphone not connected!");  
+		} else { //At least one microphone is present  
+			//Set 'micConnected' to true  
+			micConnected = true;  
   
-            //Get the default microphone recording capabilities  
-            Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);  
+			//Get the default microphone recording capabilities  
+			Microphone.GetDeviceCaps (null, out minFreq, out maxFreq);  
   
-            //According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...  
-            if(minFreq == 0 && maxFreq == 0)  
-            {  
-                //...meaning 44100 Hz can be used as the recording sampling rate  
-                maxFreq = 44100;  
-            }  
+			//According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...  
+			if (minFreq == 0 && maxFreq == 0) {  
+				//...meaning 44100 Hz can be used as the recording sampling rate  
+				maxFreq = 44100;  
+			}  
   
-            //Get the attached AudioSource component  
-            goAudioSource = this.GetComponent<AudioSource>();  
-        }  
-    }  
+			//Get the attached AudioSource component  
+			goAudioSource = this.GetComponent<AudioSource> ();  
+		}  
+		Invoke ("sampleAgain", 10.0f);
+	}  
+    
+    
+	void Update ()
+	{
+		if (!waitingForNextSample && micConnected && (null != goAudioSource) && (null != goAudioSource.clip)) {
+			waitingForNextSample = true;
+			float[] audioData = new float[goAudioSource.clip.samples * goAudioSource.clip.channels];
+			goAudioSource.clip.GetData (audioData, 0);
+			Debug.Log ("Got data.");
+			foreach (float i in audioData) {
+				Debug.Log (i);
+			}
+			Debug.Log ("===============\n\n");
+			Invoke ("sampleAgain", 10.0f);
+		} /*else if (micConnected && (null != goAudioSource)) {
+			Debug.Log ("goAudioSource = " + goAudioSource);
+		}*/
+	}
+	
+	void sampleAgain () {
+		waitingForNextSample = false;
+	}
+	
   
     void OnGUI()   
     {  
