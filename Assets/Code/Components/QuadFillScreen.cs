f99e8c33cd25edc0d5c2f8856814fb9fd154524d
Eliot <fadookie@gmail.com>
2013-01-26 16:17:02 -0800

    Switch to Unity Pro trial. Render main camera to texture which is then upscaled to create a pixellated effect for the road.

---
+++ Assets/Code/Components/QuadFillScreen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+ 
+public class QuadFillScreen:MonoBehaviour
+{
+	public Camera cam;
+	void Setup() {
+		if (null == cam) {
+		throw new System.Exception("Camera must be set");
+	}
+	
+	}
+    void Update()
+    {
+        float pos = (cam.nearClipPlane + 0.01f);
+ 
+        transform.position = cam.transform.position + cam.transform.forward * pos;
+ 
+        float h = Mathf.Tan(cam.fov*Mathf.Deg2Rad*0.5f)*pos*2f;
+ 
+        transform.localScale = new Vector3(h*cam.aspect,h,0f);
+    }
+}
