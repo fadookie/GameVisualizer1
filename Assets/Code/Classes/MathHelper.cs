e9783fdeba4553ef713e53b153e080c1681395cc
Eliot <fadookie@gmail.com>
2013-02-02 01:36:31 -0800

    GameManager now manages state transitions between songs.

--- Assets/Code/Classes/MathHelper.cs
+++ Assets/Code/Classes/MathHelper.cs
@@ -1,20 +1,29 @@
 public class MathHelper {
 
-	/**
-   * Re-maps a number from one range to another. In the example above,
-   * the number '25' is converted from a value in the range 0..100 into
-   * a value that ranges from the left edge (0) to the right edge (width)
-   * of the screen.
-   * <br/> <br/>
-   * Numbers outside the range are not clamped to 0 and 1, because
-   * out-of-range values are often intentional and useful.
-   *
-   * @param value the incoming value to be converted
-   * @param istart lower bound of the value's current range
-   * @param istop upper bound of the value's current range
-   * @param ostart lower bound of the value's target range
-   * @param ostop upper bound of the value's target range
-   */
+   /// <summary>
+   /// Re-maps a number from one range to another. In the example above,
+   /// the number '25' is converted from a value in the range 0..100 into
+   /// a value that ranges from the left edge (0) to the right edge (width)
+   /// of the screen.
+   /// <br/> <br/>
+   /// Numbers outside the range are not clamped to 0 and 1, because
+   /// out-of-range values are often intentional and useful.
+   /// </summary>
+   /// <param name='value'>
+   /// the incoming value to be converted
+   /// </param>
+   /// <param name='istart'>
+   /// lower bound of the value's current range
+   /// </param>
+   /// <param name='istop'>
+   /// upper bound of the value's current range
+   /// </param>
+   /// <param name='ostart'>
+   /// lower bound of the value's target range
+   /// </param>
+   /// <param name='ostop'>
+   /// upper bound of the value's target range
+   /// </param>
 	public static float Map(float value,
                                 float istart, float istop,
                                 float ostart, float ostop) {
@@ -23,5 +32,19 @@ public class MathHelper {
 
 	public static float percentRemaining(float n, float total) {
 		return (n%total)/total;
+	}
+	
+	/// <summary>
+	/// Modulo function that also works on negative numbers
+	/// Taken from http://stackoverflow.com/a/1082938/350761
+	/// </summary>
+	/// <param name='x'>
+	/// First operand.
+	/// </param>
+	/// <param name='m'>
+	/// Second operand.
+	/// </param>
+	public static int Mod(int x, int m) {
+	    return (x%m + m)%m;
 	}
 }
--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -4,8 +4,33 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+[System.Serializable]
+public class SongPreset {
+	public string songName;
+	public string notes;
+	public string titleCardName;
+	public int BPM;
+	
+	public string ToString() {
+		return string.Format("SongPreset{{name:{0}, notes:{1}, titleCardName:{2}, BPM:{3}}}", songName, notes, titleCardName, BPM);
+	}
+}
+	
 public class GameManager : MonoSingleton<GameManager>
 {
+	public GameState gameState = GameState.TitleCard;
+	public SongPreset[] songPresets;
+	uint _currentPreset = 0;
+	public string skipBackKey = "[";
+	public string skipForwardKey = "]";
+	public string toggleTitleKey = "t";
+	
+	public enum GameState {
+		TitleCard = 0,
+		Visualizer
+	}
+	
+	
 	public override void Init() {
 	}
 	
@@ -13,5 +38,28 @@ public class GameManager : MonoSingleton<GameManager>
 	}
 	
 	void Update() {
+		//Process input
+		bool presetChanged = false;
+		if (Input.GetKeyDown(skipBackKey)) {
+			_currentPreset = (uint)MathHelper.Mod((int)_currentPreset - 1, songPresets.Length);
+			presetChanged = true;
+		} else if (Input.GetKeyDown(skipForwardKey)) {
+			_currentPreset = (uint)MathHelper.Mod((int)_currentPreset + 1, songPresets.Length);
+			presetChanged = true;
+		}
+		if (Input.GetKeyDown(toggleTitleKey)) {
+			gameState = (gameState == GameState.TitleCard) ? GameState.Visualizer : GameState.TitleCard;
+		}
+		
+		//Process state change
+		if (presetChanged) {
+			SongPreset currentPreset = songPresets[_currentPreset];
+			Debug.Log("Activate " + currentPreset.ToString());
+			//Push state to MovieController
+			MovieController movieController = GameObject.FindGameObjectWithTag("MovieController").GetComponent<MovieController>();
+			
+			//Push state to TempoManager
+			TempoManager.instance.pendingBPM = currentPreset.BPM;
+		}
 	}
 }
