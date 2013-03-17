553ac4c45d77c7b7bf275f773a1481fe8ebe508a
Eliot <fadookie@gmail.com>
2013-03-16 18:56:45 -0700

    Add ShooterPlayerController and players arrays, now moves around with the arrow keys.

--- Assets/Code/Components/ShooterController.cs
+++ Assets/Code/Components/ShooterController.cs
@@ -4,17 +4,31 @@ using System.Collections.Generic;
 
 public class ShooterController : Reactive {
 	
+	const uint NUM_PLAYERS = 1;
+	GameObject[] players = new GameObject[NUM_PLAYERS];
+	ShooterPlayerController[] playerControllers = new ShooterPlayerController[NUM_PLAYERS];
 	List<OTAnimatingSprite> enemies = new List<OTAnimatingSprite>();
 		
 	// Use this for initialization
 	void Start () {
-		ReactiveManager.Instance.registerListener(this, getChannels());
-		Debug.Log("This is your mom's shooter ;)");
+		//Spawn players
+		for (int i = 0; i < players.Length; i++) {
+			players[i] = OT.CreateObject("Player");
+			if (players[i] == null) throw new System.Exception("Player cannot be null");
+			players[i].transform.parent = transform;
+			players[i].AddComponent("ShooterPlayerController");
+			playerControllers[i] = players[i].GetComponent<ShooterPlayerController>();
+			if (playerControllers[i] == null) throw new System.Exception("PlayerController cannot be null");
+		}
+		
+		//Spawn initial enemies
 		GameObject enemyObject =  OT.CreateObject("SpaceInvader1");
 		enemyObject.transform.parent = transform;
 		OTAnimatingSprite enemySprite = enemyObject.GetComponent<OTAnimatingSprite>();
 		if(null == enemySprite) throw new System.Exception("Enemy object must have an OTAnimatingSprite");
 		enemies.Add(enemySprite);
+		
+		ReactiveManager.Instance.registerListener(this, getChannels());
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Code/Components/ShooterPlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class ShooterPlayerController : Reactive {
+	public string horizontalAxisName = "Horizontal";
+	public string verticalAxisName = "Vertical";
+	public float moveAmount = 3.5f;
+	OTAnimatingSprite sprite = null;
+
+	// Use this for initialization
+	void Start () {
+		sprite = gameObject.GetComponent<OTAnimatingSprite>();
+		if (sprite == null) throw new System.Exception("Sprite cannot be null.");
+		
+		ReactiveManager.Instance.registerListener(this, getChannels());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalAxis = Input.GetAxis(horizontalAxisName);
+		float verticalAxis = Input.GetAxis(verticalAxisName);
+		
+		horizontalAxis *= moveAmount;
+		verticalAxis *= moveAmount;
+		
+		sprite.position = new Vector2(sprite.position.x + horizontalAxis, sprite.position.y + verticalAxis);
+	}
+	
+	#region Reactive event handlers
+	
+	public override void reactToAmplitude(uint channel, float amp, bool overThreshold) {
+	}
+	
+	public override void reactToBeat(float currentBPM) {
+	}
+	
+	#endregion	
+}
